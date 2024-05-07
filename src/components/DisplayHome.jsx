a774814ed17a7630518f4e4b503148b3a45e2cab
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-07 17:40:25 +0300

    todays biggest hits section added

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./Navbar";
 import { albumsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
+import { songsData } from "../assets/assets";
+import SongItem from "../components/SongItem";
 
 const DisplayHome = () => {
   return (
@@ -12,6 +14,20 @@ const DisplayHome = () => {
         <div className="flex overflow-auto">
           {albumsData.map((item, index) => (
             <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
               key={index}
               name={item.name}
               desc={item.desc}
