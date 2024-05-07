2fa6aea2c93d0217e16fcddbc1c98b5ba11d7bc1
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-07 16:53:50 +0300

    Featured Charts section added to display home page

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
