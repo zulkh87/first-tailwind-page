0ce9f6567a82845051eed4c4683fe28fadb869b3
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-07 16:47:38 +0300

    added navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,6 +29,15 @@ const Navbar = () => {
           </p>
         </div>
       </div>
+      <div className="flex items-center gap-2 mt-4">
+        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">
+          All
+        </p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">
+          Podcasts
+        </p>
+      </div>
     </>
   );
 };
