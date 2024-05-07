ec6ba15e75e90db6a28c05672e447d85685bcc32
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-07 13:25:11 +0300

    added Display Home component

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const DisplayHome = () => {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default DisplayHome;
