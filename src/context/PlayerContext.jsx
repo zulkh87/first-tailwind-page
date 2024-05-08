8aa09fea4a0ef70b3d9b696a539ac1819b9f04d9
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-08 15:14:08 +0300

    player context bug fix

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -5,7 +5,11 @@ export const PlayerContext = createContext();
 const PlayerContextProvider = (props) => {
   const contextValue = {};
 
-  return <PlayerContext value={contextValue}>{props.children}</PlayerContext>;
+  return (
+    <PlayerContext.Provider value={contextValue}>
+      {props.children}
+    </PlayerContext.Provider>
+  );
 };
 
 export default PlayerContextProvider;
