8fc6c29bc15ad2d1e21dab7eb53072f66c697703
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-08 16:50:51 +0300

    music player functional except play|pause button

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,9 +1,75 @@
-import { createContext } from "react";
+import { createContext, useRef, useState, useEffect } from "react";
+import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
 
 const PlayerContextProvider = (props) => {
-  const contextValue = {};
+  const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
+
+  const [track, setTrack] = useState(songsData[0]);
+  const [playStatus, setPlayStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {
+      second: 0,
+      minute: 0,
+    },
+    totalTime: {
+      second: 0,
+      minute: 0,
+    },
+  });
+
+  const play = () => {
+    audioRef.current.play();
+    setPlayStatus(true);
+  };
+
+  const pause = () => {
+    audioRef.current.pause();
+    setPlayStatus(false);
+  };
+
+  const playWithId = async (id) => {
+    await setTrack(songsData[id]);
+    await audioRef.current.play();
+    setPlayStatus(true);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      audioRef.current.ontimeupdate = () => {
+        seekBar.current.style.width =
+          Math.floor(
+            (audioRef.current.currentTime / audioRef.current.duration) * 100
+          ) + "%";
+        setTime({
+          currentTime: {
+            second: Math.floor(audioRef.current.currentTime % 60),
+            minute: Math.floor(audioRef.current.currentTime / 60),
+          },
+          totalTime: {
+            second: Math.floor(audioRef.current.duration % 60),
+            minute: Math.floor(audioRef.current.duration % 60),
+          },
+        });
+      };
+    }, 1000);
+  }, [audioRef]);
+
+  const contextValue = {
+    audioRef,
+    seekBar,
+    seekBg,
+    track,
+    setTrack,
+    playStatus,
+    setPlayStatus,
+    time,
+    setTime,
+    playWithId,
+  };
 
   return (
     <PlayerContext.Provider value={contextValue}>
